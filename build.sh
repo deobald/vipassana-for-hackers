c42b2cbe44aee6cd843c1f31fa542bff1722fb70
Steven Deobald <steven@nilenso.com>
2017-11-11 19:51:55 +0530

    switching from pdflatex to xelatex... hopefully this does not ruin too many things

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-pdflatex vipassana-for-hackers.tex
+#!/bin/sh
+
+# pdflatex was used originally but cannot render Sanskrit without a lot of hoop-jumping
+# pdflatex vipassana-for-hackers.tex
+
+# xelatex is apparently less capable than pdflatex at font-rendering in general, but
+# is capable of rendering non-Latin fonts (like Hindi/Sanskrit) by default
+
+xelatex vipassana-for-hackers.tex
+
