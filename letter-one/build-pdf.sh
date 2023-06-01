b301ddf7b290723efbba970d53975736f808de39
Steven Deobald <steven@deobald.ca>
2023-06-01 13:59:28 -0300

    Adding Letter One (On Gurudom)

---
+++ letter-one/build-pdf.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# pdflatex was used originally but cannot render Sanskrit without a lot of hoop-jumping
+# pdflatex vipassana-for-hackers.tex
+
+# xelatex is apparently less capable than pdflatex at font-rendering in general, but
+# is capable of rendering non-Latin fonts (like Hindi/Sanskrit) by default
+
+# Compile twice to resolve references.
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+LIBPATH="$SCRIPTPATH/../lib"
+PATH=$PATH:$LIBPATH
+
+OUT=../output
+
+echo "Running pre-build. This takes a while..."
+xelatex -output-directory=$OUT letter-one.tex # > /dev/null 2>&1
+echo "...done. Now running the build."
+xelatex -output-directory=$OUT letter-one.tex
+
+rm -f texput.log $OUT/*.aux $OUT/*.log $OUT/*.out $OUT/*.tdo $OUT/*.bib
+
